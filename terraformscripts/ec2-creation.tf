ae66d3cb74abbb76e2bb64c67d17782a807beb28
Shahaji Jagdale <shahaji2201@gmail.com>
2023-01-03 22:00:45 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "AWSEC2Instance"{
      count   = 1
      ami = "ami-0f9d9a251c1a44858"
      instance_type = "t2.micro"
-     security_groups = ["launch-wizard-6"]
+     security_groups = ["launch-wizard-18"]
      key_name        = "devopsmss"
      tags = {
         Name = "tomcatservers"
