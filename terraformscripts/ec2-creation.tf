b8ad86add6680db61f755884459644cfa8b33fc4
Shahaji Jagdale <shahaji1807@gmail.com>
2023-01-03 18:13:32 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 resource "aws_instance" "AWSEC2Instance"{
      count   = 1
-     ami = "ami-010d564ad44bd1912"
+     ami = "ami-0f9d9a251c1a44858"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
      key_name        = "devopsmss"
